376d10694ebad56a57d9d484ae42a5df58b2ae09
Shin Jinwoo <bluesky0255@naver.com>
2018-03-27 16:01:03 +0900

    DB 객체 추상화 작성
    - save 메서드 갖는 dbo 인터페이스 작성
    - 회원 dbo 작성(save 구현 필요함)

---
+++ server/src/db/user.dbo.ts
@@ -0,0 +1,46 @@
+import { IDBO } from "./dbo.interface";
+import { RoleDBO } from "./role.dbo";
+
+export class UserDBO implements IDBO {
+
+	public constructor(
+		private id: string | number,
+		private sid: string, 
+		private name: string, 
+		private password: string, 
+		private salt: string, 
+		private role: RoleDBO) {}
+
+	public getId() {
+		return this.id;
+	}
+
+	public getName() {
+		return this.name;
+	}
+
+	public getSID() {
+		return this.sid;
+	}
+
+	public getPassword() {
+		return this.password;
+	}
+
+	public getSalt() {
+		return this.salt;
+	}
+
+	public setPassword(password: string) {
+		this.password = password;
+	}
+
+	public setRole(role: RoleDBO) {
+		this.role = role;
+	}
+
+	public save() {
+		// TODO: implement save entire properties up to db interface
+		return null;
+	}
+}
