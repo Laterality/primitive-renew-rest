0c9ee6211d68396a2895d77d2ac5b65fa92435d1
Shin Jinwoo <bluesky0255@naver.com>
2018-03-29 16:34:58 +0900

    role check 함수 분리

---
+++ server/src/lib/session-handler.ts
@@ -0,0 +1,15 @@
+import * as express from "express";
+
+import { IDatabase } from "../db/db-interface";
+
+export async function checkRole(db: IDatabase, req: express.Request, roleTitle: string | string[]): Promise<boolean> {
+	if (!req.session) { return false; }
+	const userSess = await db.findUserById(req.session["userId"]);
+	if (!userSess) { return false; }
+	if (Array.isArray(roleTitle)) {
+		return roleTitle.indexOf(userSess.getRole().getTitle()) > -1;
+	}
+	else {
+		return userSess.getRole().getTitle() === roleTitle;
+	}
+}
