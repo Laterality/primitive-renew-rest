473c1911e0530a85bf33d5f037ad4fa83cfa4e1f
Shin Jinwoo <bluesky0255@naver.com>
2018-04-13 14:12:39 +0900

    Front 코드 추가
    Front 개발을 위한 stub 코드와 dependency 추가

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/**
+ * Frontend Index
+ * 
+ * author: Jin-woo Shin
+ * date: 2018-04-13
+ */
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+	<h1>Typescript React Stub</h1>,
+	document.getElementById("root"),
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	entry: "./src/index.tsx",
+	output: {
+		filename: "bundle.js",
+		path: __dirname + "/public/dist"
+	},
+	devtool: "source-map",
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: [".ts", ".tsx", ".js", ".json"]
+	},
+	module: {
+		rules: [
+			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+			{ test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+			// All output '.js' files have any sourcemaps re-processed by 'source-map-loader'.
+			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+		]
+	},
+
+	// When importing a module whose path matches one of the following, just
+	// asssume a corresponding global variable exists and use that instead.
+	// This is important because it allows us to avoid bundling all of our
+	// dpendencies, which allows browsers to cahce those libraries between builds.
+	externals: {
+		"react": "React",
+		"react-dome": "ReactDOM"
+	},
+}
+
