d30aec119fc2526de83868e9bdb75ca8cb363d98
Shin Jinwoo <bluesky0255@naver.com>
2018-04-17 00:01:18 +0900

    라우터 수정
    init 함수에 try-catch 블록 적용

--- server/route/api/router.ts
+++ server/route/api/router.ts
@@ -24,37 +24,50 @@ export class APIRouter {
 		this.router.use("/v1", new V1API(this.db).getRouter());
 		this.router.post("/init", async (req: express.Request, res: express.Response) => {
 			// 관리자 계정이 존재하는지 확인
-			const rootUser = await this.db.findUserBySID("root");
-			if (rootUser) {
-				return resHandler.response(res,
-				new resHandler.ApiResponse(
-					resHandler.ApiResponse.CODE_CONFLICT,
-					resHandler.ApiResponse.RESULT_FAIL,
-					"initialization is already done"));
-			}
+			try {
+				try {
+					const rootUser = await this.db.findUserBySID("root");
+
+					if (rootUser) {
+						return resHandler.response(res,
+						new resHandler.ApiResponse(
+							resHandler.ApiResponse.CODE_CONFLICT,
+							resHandler.ApiResponse.RESULT_FAIL,
+							"initialization is already done"));
+					}
+				}
+				catch (e) {
+					if (e["message"] === "not found") {
+
+						// 역할 생성
+						await this.db.createRole(new RoleDBO("신입생"));
+						await this.db.createRole(new RoleDBO("재학생"));
+						await this.db.createRole(new RoleDBO("졸업생"));
+						const roleAdmin = await this.db.createRole(new RoleDBO("관리자"));
 			
-			// 역할 생성
-			await this.db.createRole(new RoleDBO("신입생"));
-			await this.db.createRole(new RoleDBO("재학생"));
-			await this.db.createRole(new RoleDBO("졸업생"));
-			const roleAdmin = await this.db.createRole(new RoleDBO("관리자"));
-
-			// 게시판 생성
-			const seminarPermitted: RoleDBO[] = [];
-			seminarPermitted.push(await this.db.findRoleByTitle("신입생") as RoleDBO);
-			seminarPermitted.push(await this.db.findRoleByTitle("재학생") as RoleDBO);
-			seminarPermitted.push(await this.db.findRoleByTitle("졸업생") as RoleDBO);
-			seminarPermitted.push(await this.db.findRoleByTitle("관리자") as RoleDBO);
-
-			await this.db.createBoard(new BoardDBO("세미나", seminarPermitted, seminarPermitted));
-			await this.db.createBoard(new BoardDBO("과제", seminarPermitted, seminarPermitted));
-			await this.db.createBoard(new BoardDBO("신입생 자료실", seminarPermitted, seminarPermitted));
-
-			// 관리자 계정 생성
-			const authInfo = await encryption("root");
-			this.db.createUser(new UserDBO("root", "관리자", authInfo[0], authInfo[1], roleAdmin as RoleDBO));
-
-			return resHandler.response(res, resHandler.createOKResponse());
+						// 게시판 생성
+						const seminarPermitted: RoleDBO[] = [];
+						seminarPermitted.push(await this.db.findRoleByTitle("신입생") as RoleDBO);
+						seminarPermitted.push(await this.db.findRoleByTitle("재학생") as RoleDBO);
+						seminarPermitted.push(await this.db.findRoleByTitle("졸업생") as RoleDBO);
+						seminarPermitted.push(await this.db.findRoleByTitle("관리자") as RoleDBO);
+			
+						await this.db.createBoard(new BoardDBO("세미나", seminarPermitted, seminarPermitted));
+						await this.db.createBoard(new BoardDBO("과제", seminarPermitted, seminarPermitted));
+						await this.db.createBoard(new BoardDBO("신입생 자료실", seminarPermitted, seminarPermitted));
+			
+						// 관리자 계정 생성
+						const authInfo = await encryption("root");
+						this.db.createUser(new UserDBO("root", "관리자", authInfo[0], authInfo[1], roleAdmin as RoleDBO));
+			
+						return resHandler.response(res, resHandler.createOKResponse());
+					}
+					else { throw e; }
+				}
+			}
+			catch (e) {
+				console.log(e);
+			}
 		});
 	}
 
