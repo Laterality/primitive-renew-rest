9ae3153f9354cde51eef75e54bf53b04df3fd622
Shin Jinwoo <bluesky0255@naver.com>
2018-04-22 10:33:57 +0900

    DELETE frontend code
    - They moved separate Repo.
    - ref. [https://github.com/Laterality/primitive-renew-web]

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "../server/config";
-
-export class Routes {
-	public static readonly routeRoot = `/`;
-	public static readonly routeBoard = `/board`;
-	public static readonly routeWrite = `/write`;
-	public static readonly routePost = `/post`;
-	
-}
